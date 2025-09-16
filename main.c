f9677398dba76c2df1a3af3aa8ba0e24c569d099
JudeLingan <judecohenlingan@gmail.com>
2025-09-16 15:26:24 -0400

    reworked main function to prevent some duplicate code

--- main.c
+++ main.c
@@ -49,84 +49,72 @@ int main() {
 
 		stringlist input = stringlist_input();
 
-		if (input.items == NULL || input.items[0] == 0) {
-			printf("ERROR: null input\n");
-			continue;
-		}
-		else if (!strcmp(input.items[0], "new")) {
-			printf("Enter name of task: ");
-			char *taskname = get_input();
-			printf("Enter any details: ");
-			char *tasktext = get_input();
+		switch (input.length) {
+			case 0:
+				printf("ERROR: null input\n");
+				break;
+			case 1:
+				if (!strcmp(input.items[0], "new")) {
+					printf("Enter id of parent task or \"0\" for root: ");
+					int64_t id = atoi(get_input());
+					printf("Enter name of task: ");
+					char *taskname = get_input();
+					printf("Enter any details: ");
+					char *tasktext = get_input();
 
-			task *parent;
+					task *parent = tasktree_get_task_by_id(id);
 
-			if (input.length == 2) {
-				parent = tasktree_get_task_by_id(atoi(input.items[1]));
-			}
-			else {
-				parent = NULL;
-			}
+					task tsk = new_task(taskname, tasktext, -1);
 
-			task tsk = new_task(taskname, tasktext, -1);
+					tasktree_add_task(tsk, parent);
 
-			tasktree_add_task(tsk, parent);
+					task_free_elements(tsk);
+					free(taskname);
+					free(tasktext);
+				}
+				else if (!strcmp(input.items[0], "list")) {
+					tasktree_print();
+				}
+				else if (!strcmp(input.items[0], "done")) {
+					stringlist_free_elements(input);
+					tasktree_unload();
 
-			task_free_elements(tsk);
-			free(taskname);
-			free(tasktext);
-		}
-		else if (!strcmp(input.items[0], "list")) {
-			tasktree_print();
-		}
-		else if (!strcmp(input.items[0], "get")) {
-			if (input.length < 2) {
-				printf("'get' command requires a task id\n");
-			}
-			else {
-				int64_t id = atoi(input.items[1]);
-				task *tsk = tasktree_get_task_by_id(id);
-				if (tsk == NULL) {
-					printf("task %s not found\n", input.items[1]);
+					return 0;
 				}
 				else {
-					print_task(*tsk);
+					printf("command not found or wrong number of arguments");
 				}
-			}
-		}
-		else if (!strcmp(input.items[0], "complete")) {
-			if (input.length < 2) {
-				printf("'get' command requires a task path\n");
-			}
-			else {
-				int64_t id = atoi(input.items[1]);
-				task *tsk = tasktree_get_task_by_id(id);
-				if (tsk == NULL) {
-					printf("task %s not found\n", input.items[1]);
+				break;
+			case 2:
+				if (!strcmp(input.items[0], "get")) {
+					int64_t id = atoi(input.items[1]);
+					task *tsk = tasktree_get_task_by_id(id);
+					if (tsk == NULL) {
+						printf("task %s not found\n", input.items[1]);
+					}
+					else {
+						print_task(*tsk);
+					}
+				}
+				else if (!strcmp(input.items[0], "complete")) {
+					int64_t id = atoi(input.items[1]);
+					task *tsk = tasktree_get_task_by_id(id);
+					if (tsk == NULL) {
+						printf("task %s not found\n", input.items[1]);
+					}
+					else {
+						task_toggle_complete(tsk);
+					}
+				}
+				else if (!strcmp(input.items[0], "modify")) {
+
+				}
+				else if (!strcmp(input.items[0], "remove")) {
+					tasktree_remove_task(tasktree_get_task_by_id(atoi(input.items[1])));
 				}
 				else {
-					task_toggle_complete(tsk);
+					printf("command not found or wrong number of arguments");
 				}
-			}
-		}
-		else if (!strcmp(input.items[0], "remove")) {
-			if (input.length < 2) {
-				printf("'remove' command requires a task path\n");
-			}
-			else {
-				tasktree_remove_task(tasktree_get_task_by_id(atoi(input.items[1])));
-			}
-		}
-		else if (!strcmp(input.items[0], "done")) {
-
-			stringlist_free_elements(input);
-
-			tasktree_unload();
-
-			break;
-		}
-		else {
-			printf("command %s not found\n", input.items[0]);
 		}
 
 		stringlist_free_elements(input);
