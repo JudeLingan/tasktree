999eae9ee462a9ac484f6d6f70257b5561d23818
JudeLingan <judecohenlingan@gmail.com>
2025-12-28 12:12:47 -0500

    revamped testing to use CUnit

--- src/test.c
+++ src/test.c
@@ -2,9 +2,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <assert.h>
 #include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
 #include "util.h"
 #include "tasktree.h"
+
+/* GLOBAL MACROS */
+#define STR(x) #x
+#define CONCAT(x, y) x##y
 
 static int total_tests = 0;
 static int passed_tests = 0;
@@ -39,21 +45,37 @@ char* test_malloc_snprintf(void* val) {
 	return out;
 }
 
-char* test_is_pure_num(void* val) {
-	if (!is_pure_num("0123456789")) {
-		return "0123456789 NOT PURE NUM";
-	}
-	if (is_pure_num("a0123456789")) {
-		return "a0123456789 PURE NUM";
-	}
-	if (is_pure_num("0123456789a")) {
-		return "a0123456789 PURE NUM";
-	}
-	return NULL;
+void test_is_pure_num() {
+	CU_ASSERT_TRUE(is_pure_num("0123456789"));
+	CU_ASSERT_FALSE(is_pure_num("a0123456789"));
+	CU_ASSERT_FALSE(is_pure_num("0123456789a"));
+	CU_ASSERT_FALSE(is_pure_num("012abc3456789"));
+	CU_ASSERT_FALSE(is_pure_num(""));
+	CU_ASSERT_FALSE(is_pure_num(NULL));
 }
 
 int main() {
-	run_test(test_malloc_snprintf, NULL);
-	run_test(test_is_pure_num, NULL);
-	return 0;
+#define ADD_TEST(s, t) CU_add_test((s), STR(t), CONCAT(test_, t))
+
+	if (CU_initialize_registry() != CUE_SUCCESS) {
+		goto out;
+	}
+
+	CU_pSuite util_suite = CU_add_suite("util", NULL, NULL);
+	if (util_suite == NULL) {
+		CU_cleanup_registry();
+		goto out;
+	}
+
+	ADD_TEST(util_suite, is_pure_num);
+
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+	CU_basic_run_suite(util_suite);
+	printf("%d\n", CU_get_number_of_suites_run());
+
+	CU_cleanup_registry();
+
+out:
+	printf("testing complete");
+	return CU_get_error();
 }
